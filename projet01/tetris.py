ca6037c6f19804ee10f67d269b078bcd31c6676d
Wyatt Lacroix <wyatt.lacroix@etu.univ-orleans.fr>
2025-09-12 10:45:04 +0200

    Finished changing functions and methods to respect PEP8 style, started changing variables names and adding commentaries

--- projet01/tetris.py
+++ projet01/tetris.py
@@ -20,7 +20,7 @@ import random
 import time
 import pygame
 import sys
-import constantes
+from constantes import *
 
 
 
@@ -44,31 +44,31 @@ class Jeu:
     """Démarrage et arrêt du jeu"""
 
     def start(self):
-        self._afficherTexte('Tetris', CENTRE_FENETRE, font='titre')
-        self._afficherTexte('Appuyer sur une touche...', POS)
-        self._attente()
+        self.afficher_texte('Tetris', CENTRE_FENETRE, font='titre')
+        self.afficher_texte('Appuyer sur une touche...', POS)
+        self.attente()
 
     def stop(self):
-        self._afficherTexte('Perdu', CENTRE_FENETRE, font='titre')
-        self._attente()
-        self._quitter()
+        self.afficher_texte('Perdu', CENTRE_FENETRE, font='titre')
+        self.attente()
+        self.quitter()
 
-    def _afficherTexte(self, text, position, couleur=9, font='defaut'):
+    def afficher_texte(self, text, position, couleur=9, font='defaut'):
         #		print("Afficher Texte")
         font = self.fonts.get(font, self.fonts['defaut'])
         couleur = COULEURS.get(couleur, COULEURS[9])
         rendu = font.render(text, True, couleur)
-        rect = rendu.get_rect()
-        rect.center = position
-        self.surface.blit(rendu, rect)
+        rectangle = rendu.get_rect() # get_rect() is used to obtain a rectangle object
+        rectangle.center = position
+        self.surface.blit(rendu, rectangle)  #blit() put content of rendu on surface at the position given by rectangle 
 
-    def _getEvent(self):
+    def get_event(self):
         for event in pygame.event.get():
             if event.type == QUIT:
-                self._quitter()
+                self.quitter()
             if event.type == KEYUP:
                 if event.key == K_ESCAPE:
-                    self._quitter()
+                    self.quitter()
             if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     continue
@@ -76,31 +76,31 @@ class Jeu:
 
     """Quitter le jeu"""
 
-    def _quitter(self):
+    def quitter(self):
         print("Quitter")
         pygame.quit()
         sys.exit()
 
-    def _rendre(self):
+    def rendre(self):
         pygame.display.update()
         self.clock.tick()
 
-    def _attente(self):
+    def attente(self):
         print("Attente")
-        while self._getEvent() == None:
-            self._rendre()
+        while self.get_event() == None:
+            self.rendre()
 
-    def _getPiece(self):
+    def get_piece(self):
         return PIECES.get(random.choice(PIECES_KEYS))
 
-    def _getCurrentPieceColor(self):
-        for l in self.current[0]:
-            for c in l:
-                if c != 0:
-                    return c
+    def get_current_piece_color(self): # to del, previously row was l and cell was c
+        for row in self.current[0]:
+            for cell in row:
+                if cell != 0:
+                    return cell
         return 0
 
-    def _calculerDonneesPieceCourante(self):
+    def calculer_donnes_piece_courante(self):
         m = self.current[self.position[2]]
         coords = []
         for i, l in enumerate(m):
@@ -109,7 +109,7 @@ class Jeu:
                     coords.append([i + self.position[0], j + self.position[1]])
         self.coordonnees = coords
 
-    def _estValide(self, x=0, y=0, r=0):
+    def est_valide(self, x=0, y=0, r=0):
         max_x, max_y = DIM_PLATEAU
         if r == 0:
             coordonnees = self.coordonnees
@@ -141,12 +141,12 @@ class Jeu:
 #		print("Position testée valide: x=%s, y=%s" % (x, y))
         return True
 
-    def _poserPiece(self):
+    def poser_piece(self):
         print("La pièce est posée")
         if self.position[1] <= 0:
             self.perdu = True
         # Ajout de la pièce parmi le plateau
-        couleur = self._getCurrentPieceColor()
+        couleur = self.get_current_piece_color()
         for cx, cy in self.coordonnees:
             self.plateau[cy][cx] = couleur
         completees = []
@@ -174,72 +174,72 @@ class Jeu:
         # Travail avec la pièce courante terminé
         self.current = None
 
-    def _first(self):
+    def first(self):
         self.plateau = [[0] * DIM_PLATEAU[0] for i in range(DIM_PLATEAU[1])]
         self.score, self.pieces, self.lignes, self.tetris, self.niveau = 0, 0, 0, 0, 1
-        self.current, self.next, self.perdu = None, self._getPiece(), False
+        self.current, self.suivante, self.perdu = None, self.get_piece(), False
 
-    def _next(self):
+    def next(self):
         print("Piece suivante")
-        self.current, self.next = self.next, self._getPiece()
+        self.current, self.suivante = self.suivante, self.get_piece()
         self.pieces += 1
         self.position = [int(DIM_PLATEAU[0] / 2) - 2, -4, 0]
-        self._calculerDonneesPieceCourante()
+        self.calculer_donnes_piece_courante()
         self.dernier_mouvement = self.derniere_chute = time.time()
 
-    def _gererEvenements(self):
-        event = self._getEvent()
+    def gerer_evenements(self):
+        event = self.get_event()
         if event == K_p:
             print("Pause")
             self.surface.fill(COULEURS.get(0))
-            self._afficherTexte('Pause', CENTRE_FENETRE, font='titre')
-            self._afficherTexte('Appuyer sur une touche...', POS)
-            self._attente()
+            self.afficher_texte('Pause', CENTRE_FENETRE, font='titre')
+            self.afficher_texte('Appuyer sur une touche...', POS)
+            self.attente()
         elif event == K_LEFT:
             print("Mouvement vers la gauche")
-            if self._estValide(x=-1):
+            if self.est_valide(x=-1):
                 self.position[0] -= 1
         elif event == K_RIGHT:
             print("Mouvement vers la droite")
-            if self._estValide(x=1):
+            if self.est_valide(x=1):
                 self.position[0] += 1
         elif event == K_DOWN:
             print("Mouvement vers le bas")
-            if self._estValide(y=1):
+            if self.est_valide(y=1):
                 self.position[1] += 1
         elif event == K_UP:
             print("Mouvement de rotation")
-            if self._estValide(r=1):
+            if self.est_valide(r=1):
                 self.position[2] = (self.position[2] + 1) % len(self.current)
         elif event == K_SPACE:
             print("Mouvement de chute %s / %s" %
                   (self.position, self.coordonnees))
             if self.position[1] <= 0:
                 self.position[1] = 1
-                self._calculerDonneesPieceCourante()
+                self.calculer_donnes_piece_courante()
             a = 0
-            while self._estValide(y=a):
+            while self.est_valide(y=a):
                 a += 1
             self.position[1] += a - 1
-        self._calculerDonneesPieceCourante()
+        self.calculer_donnes_piece_courante()
 
-    def _gererGravite(self):
+    def gerer_gravite(self):
         if time.time() - self.derniere_chute > 0.35:
             self.derniere_chute = time.time()
-            if not self._estValide():
+            if not self.est_valide():
                 print("On est dans une position invalide")
                 self.position[1] -= 1
-                self._calculerDonneesPieceCourante()
-                self._poserPiece()
-            elif self._estValide() and not self._estValide(y=1):
-                self._calculerDonneesPieceCourante()
-                self._poserPiece()
+                self.calculer_donnes_piece_courante()
+                self.poser_piece()
+            elif self.est_valide() and not self.est_valide(y=1):
+                self.calculer_donnes_piece_courante()
+                self.poser_piece()
             else:
                 print("On déplace vers le bas")
                 self.position[1] += 1
-                self._calculerDonneesPieceCourante()
+                self.calculer_donnes_piece_courante()
 
-    def _dessinerPlateau(self):
+    def dessiner_plateau(self):
         self.surface.fill(COULEURS.get(0))
         pygame.draw.rect(self.surface, COULEURS[8],
                          START_PLABORD + TAILLE_PLABORD, BORDURE_PLATEAU)
@@ -255,7 +255,7 @@ class Jeu:
                                  coordonnees + TAILLE_BLOC)
         if self.current is not None:
             for position in self.coordonnees:
-                couleur = COULEURS.get(self._getCurrentPieceColor())
+                couleur = COULEURS.get(self.get_current_piece_color())
                 coordonnees = tuple([
                     START_PLATEAU[k] + position[k] * TAILLE_BLOC[k]
                     for k in range(2)
@@ -263,24 +263,24 @@ class Jeu:
                 pygame.draw.rect(self.surface, couleur,
                                  coordonnees + TAILLE_BLOC)
         self.score, self.pieces, self.lignes, self.tetris, self.niveau  #TODO
-        self._afficherTexte('Score: >%s' % self.score, POSITION_SCORE)
-        self._afficherTexte('Pièces: %s' % self.pieces, POSITION_PIECES)
-        self._afficherTexte('Lignes: %s' % self.lignes, POSITION_LIGNES)
-        self._afficherTexte('Tetris: %s' % self.tetris, POSITION_TETRIS)
-        self._afficherTexte('Niveau: %s' % self.niveau, POSITION_NIVEAU)
+        self.afficher_texte('Score: >%s' % self.score, POSITION_SCORE)
+        self.afficher_texte('Pièces: %s' % self.pieces, POSITION_PIECES)
+        self.afficher_texte('Lignes: %s' % self.lignes, POSITION_LIGNES)
+        self.afficher_texte('Tetris: %s' % self.tetris, POSITION_TETRIS)
+        self.afficher_texte('Niveau: %s' % self.niveau, POSITION_NIVEAU)
 
-        self._rendre()
+        self.rendre()
 
     def play(self):
         print("Jouer")
         self.surface.fill(COULEURS.get(0))
-        self._first()
+        self.first()
         while not self.perdu:
             if self.current is None:
-                self._next()
-            self._gererEvenements()
-            self._gererGravite()
-            self._dessinerPlateau()
+                self.next()
+            self.gerer_evenements()
+            self.gerer_gravite()
+            self.dessiner_plateau()
 
 if __name__ == '__main__':
     j = Jeu()
